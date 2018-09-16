84b4fd83e740580e4f9901e9fefda83b68ce5c8d
5875616E <40747311+5875616E@users.noreply.github.com>
2018-09-16 10:32:45 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install libestools2.5 stardict stardict-common stardict-gtk stardict-plugin stardict-plugin-espeak stardict-plugin-festival
-
-sudo cp * /usr/share/stardict/dic
-
-stardict
-
-sudo apt-get install goldendic
