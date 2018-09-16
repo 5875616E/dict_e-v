d8c45104503c631baf38fb6436ee441fefb3a093
5875616E <xuantdmi@gmail.com>
2018-09-16 10:27:59 +0700

    commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+sudo apt-get install libestools2.5 stardict stardict-common stardict-gtk stardict-plugin stardict-plugin-espeak stardict-plugin-festival
+
+sudo cp * /usr/share/stardict/dic
+
+stardict
+
+sudo apt-get install goldendic
